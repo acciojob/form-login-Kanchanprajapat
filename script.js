76d346ab075e74965170c06585197d1291d84ff6
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-06-26 10:58:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var form = document.getElementById('yourFormId');
+	var firstName = form.elements['firstName'].value;
+	var lastName = form.elements['lastName'].value;
+	alert(firstName + " "+ lastName);
 
 }
