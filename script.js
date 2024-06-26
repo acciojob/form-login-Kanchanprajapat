c513b2d081f2cb15bd013e7bdb7d6a723536d558
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-06-26 11:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function getFormvalue() {
     //Write your code here
-	var form = document.getElementById('form1');
-	var firstName = form.elements['fname'].value;
-	var lastName = form.elements['lname'].value;
-	alert(fname + " "+ lname);
+	 // Prevent the form from submitting and refreshing the page
+  event.preventDefault();
+
+  // Get the form element
+  const form = document.getElementById('form1');
+
+  // Get the values of first name and last name from the form
+  const firstName = form.elements['firstName'].value;
+  const lastName = form.elements['lastName'].value;
+
+  // Display the first and last name using alert
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
+}
+
+// Event listener to call getFormvalue() when the form is submitted
+document.getElementById('form1').addEventListener('submit', getFormvalue);
 
 }
